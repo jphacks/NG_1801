a291daa1e8a091fbd2d137cb333d6c1460986394
Yuki Kurita <y.kurita@conol.jp>
2018-10-20 21:16:26 +0900

    スプラッシュ画面の完成

--- health_tech/SettingViewController.swift
+++ health_tech/SettingViewController.swift
@@ -1,9 +1,26 @@
 import UIKit
+import RealmSwift
 
 class SettingViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
     
     // 選択肢
     let dataList = ["偏りのない食事がしたい", "痩せたい", "量を食べたい"]
+    let array = ["男性", "女性"]
+    
+    var user_flag = false
+    var user:User = User()
+    
+    var sex = true
+    var height:Double = 0
+    var weight:Double = 0
+    var age = 0
+    var request = 0
+    
+    var segment: UISegmentedControl = UISegmentedControl()
+    var HeightTextfield:UITextField = UITextField()
+    var WeightTextfield:UITextField = UITextField()
+    var AgeTextfield:UITextField = UITextField()
+    var picker:UIPickerView = UIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,20 +31,9 @@ class SettingViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
         //背景の色を変える
         self.view.backgroundColor = UIColor.white
         
-        //ナビゲーションバー
-        let navBar = UINavigationBar()
-        navBar.frame = CGRect(x: 0, y: appDelegate.statusBarHeight!, width: appDelegate.screenWidth!, height: 70)
-        //ナビゲーションアイテムのタイトルを設定
-        let navItem : UINavigationItem = UINavigationItem(title: "設定画面")
-        //ナビゲーションバーにアイテムを追加
-        navBar.pushItem(navItem, animated: true)
-        //Viewにナビゲーションバーを追加
-        view.addSubview(navBar)
-        
         //性別の入力
-        let array = ["男性", "女性"]
-        //UISegmentedControlのインスタンス作成
-        let segment: UISegmentedControl = UISegmentedControl(items: array as [AnyObject])
+        segment = UISegmentedControl(items: array as [AnyObject])
+        segment.selectedSegmentIndex = 0
         //segmentの位置を設定
         segment.center = CGPoint(x: self.view.frame.width / 2, y: self.view.frame.height / 2)
         segment.frame = CGRect(x: ((appDelegate.screenWidth!-appDelegate.screenWidth!*0.7)/2),y: 100,width:appDelegate.screenWidth!*0.7 ,height:50)
@@ -37,7 +43,6 @@ class SettingViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
         self.view.addSubview(segment)
         
         //身長の入力
-        let HeightTextfield:UITextField = UITextField()
         //textfieldの位置とサイズを設定
         HeightTextfield.frame = CGRect(x: ((appDelegate.screenWidth!-appDelegate.screenWidth!*0.7)/2),y: 200,width:appDelegate.screenWidth!*0.7 ,height:50)
         //アウトラインを表示
@@ -54,7 +59,6 @@ class SettingViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
         self.view.addSubview(HeightTextfield)
         
         //体重の入力
-        let WeightTextfield:UITextField = UITextField()
         //textfieldの位置とサイズを設定
         WeightTextfield.frame = CGRect(x: ((appDelegate.screenWidth!-appDelegate.screenWidth!*0.7)/2),y: 300,width:appDelegate.screenWidth!*0.7 ,height:50)
         //アウトラインを表示
@@ -71,7 +75,6 @@ class SettingViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
         self.view.addSubview(WeightTextfield)
         
         //年齢の入力
-        let AgeTextfield:UITextField = UITextField()
         //textfieldの位置とサイズを設定
         AgeTextfield.frame = CGRect(x: ((appDelegate.screenWidth!-appDelegate.screenWidth!*0.7)/2),y: 400,width:appDelegate.screenWidth!*0.7 ,height:50)
         //アウトラインを表示
@@ -90,7 +93,7 @@ class SettingViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
     
         //目的の入力
         // ピッカーの作成
-        let picker = UIPickerView(frame: CGRect(x: ((appDelegate.screenWidth!-appDelegate.screenWidth!*0.7)/2),y: 475,width:appDelegate.screenWidth!*0.7 ,height:100))
+        picker = UIPickerView(frame: CGRect(x: ((appDelegate.screenWidth!-appDelegate.screenWidth!*0.7)/2),y: 475,width:appDelegate.screenWidth!*0.7 ,height:100))
         // プロトコルの設定
         picker.delegate = self
         picker.dataSource = self
@@ -108,53 +111,123 @@ class SettingViewController: UIViewController, UIPickerViewDelegate, UIPickerVie
         SaveButton.backgroundColor = UIColor.white
         SaveButton.addTarget(self, action: #selector(SettingViewController.save(_:)), for: .touchUpInside)
         view.addSubview(SaveButton)
+        
+        //ユーザー情報取得
+        let realm = try! Realm()
+        let users = realm.objects(User.self)
+        print(users)
+        if (users.count > 0){
+            print("ユーザーデータが存在します")
+            user = users.first!
+            user_flag = true
+            if(user.sex){
+                segment.selectedSegmentIndex = 0
+            }else{
+                segment.selectedSegmentIndex = 1
+            }
+            HeightTextfield.text = String(user.height)
+            WeightTextfield.text = String(user.weight)
+            AgeTextfield.text = String(user.age)
+            picker.selectRow(user.request, inComponent: 0, animated: true)
+
+        }else{
+            print("ユーザーデータが存在しません")
+            user_flag = false
+        }
+        
+        //ナビゲーションバー
+        let navBar = UINavigationBar()
+        navBar.frame = CGRect(x: 0, y: appDelegate.statusBarHeight!, width: appDelegate.screenWidth!, height: 70)
+        //ナビゲーションアイテムのタイトルを設定
+        let navItem : UINavigationItem = UINavigationItem(title: "設定画面")
+        if(user_flag){
+            //ナビゲーションバー左のボタンを設定
+            navItem.leftBarButtonItem = UIBarButtonItem(title: "戻る", style: UIBarButtonItem.Style.plain, target: self,action:#selector(self.goToMain))
+        }
+        //ナビゲーションバーにアイテムを追加
+        navBar.pushItem(navItem, animated: true)
+        //Viewにナビゲーションバーを追加
+        view.addSubview(navBar)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    //ボタンを押した時の処理
+    //メインページに遷移
+    @objc func goToMain(_ sender: UIButton) {
+        self.present(MainViewController(), animated: true, completion: nil)
+    }
+    
+    //更新を押した時の処理
     @objc func change(segment:UISegmentedControl){
         //ボタンごとの処理をswitch文で処理
         switch segment.selectedSegmentIndex {
         //男性の時
         case 0:
-            print("男性")
+            sex = true
         //女性の時
         case 1:
-            print("女性")
+            sex = false
         default:
-            print("")
+            break
         }
     }
 
-    @objc func save(_ sender: UIButton) {// selectorで呼び出す場合Swift4からは「@objc」をつける。
-        let nextvc = MainViewController()
-        self.present(nextvc, animated: true, completion: nil)
+    @objc func save(_ sender: UIButton) {
+        height = Double(HeightTextfield.text!)!
+        weight = Double(WeightTextfield.text!)!
+        age = Int(AgeTextfield.text!)!
+        //Realmオブジェクト生成
+        let realm = try! Realm()
+        
+        //更新の時
+        if(user_flag){
+            try! realm.write {
+                user.sex = sex
+                user.height = height
+                user.weight = weight
+                user.age = age
+                user.request = request
+            }
+            let users = realm.objects(User.self)
+            print(users)
+            print("ユーザー更新完了")
+        //初回登録の時
+        }else{
+            //Realmへの書き込み
+            try! realm.write {
+                let user = User()
+                user.sex = sex
+                user.height = height
+                user.weight = weight
+                user.age = age
+                user.request = request
+                realm.add(user)
+                print("ユーザー登録完了")
+            }
+        }
+        //メイン画面へ遷移
+        self.present(MainViewController(), animated: true, completion: nil)
     }
     
     // UIPickerViewDataSource
-    
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         // 表示する列数
         return 1
     }
-    
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         // アイテム表示個数を返す
         return dataList.count
     }
-    
     // UIPickerViewDelegate
-    
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         // 表示する文字列を返す
         return dataList[row]
     }
-    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         // 選択時の処理
+        request = row
         print(dataList[row])
     }
 }
--- health_tech/SplashViewController.swift
+++ health_tech/SplashViewController.swift
@@ -14,12 +14,18 @@ class SplashViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let realm = try! Realm()
-        let foods = realm.objects(Food.self).filter("cal < 50")
-        for i in 0..<foods.count {
-            let food:Food = foods[i]
-            print(food.name)
-        }
+        // background image
+        let bg = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height))
+        bg.image = UIImage(named: "background.png")
+        bg.layer.zPosition = -1
+        self.view.addSubview(bg)
+        
+        // splash image
+        let sp = UIImageView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width/3, height:self.view.frame.size.width/3*72/102))
+        sp.image = UIImage(named: "splash.png")
+        sp.center = self.view.center
+        sp.layer.zPosition = 0
+        self.view.addSubview(sp)
         
         //AppDelegateのインスタンスを取得
         let appDelegate:AppDelegate = UIApplication.shared.delegate as! AppDelegate
@@ -29,61 +35,28 @@ class SplashViewController: UIViewController {
         // ステータスバーの高さを取得
         appDelegate.statusBarHeight = UIApplication.shared.statusBarFrame.height
         
-        let nextButton1 = UIButton(frame: CGRect(x: 100,y: 100,width: 100,height:50))
-        nextButton1.setTitle("button1", for: .normal)
-        nextButton1.setTitleColor(UIColor.black, for: .normal)
-        nextButton1.tintColor = UIColor.black
-        self.view.backgroundColor = UIColor.white
-        nextButton1.backgroundColor = UIColor.white
-        
-        nextButton1.addTarget(self, action: #selector(SplashViewController.goNext1(_:)), for: .touchUpInside)
-        view.addSubview(nextButton1)
-        
-        let nextButton2 = UIButton(frame: CGRect(x: 100,y: 300,width: 100,height:50))
-        nextButton2.setTitle("button2", for: .normal)
-        nextButton2.setTitleColor(UIColor.black, for: .normal)
-        nextButton2.tintColor = UIColor.black
-        self.view.backgroundColor = UIColor.white
-        nextButton2.backgroundColor = UIColor.white
-        
-        nextButton2.addTarget(self, action: #selector(SplashViewController.goNext2(_:)), for: .touchUpInside)
-        view.addSubview(nextButton2)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        //遅延処理
+        let dispatchTime = DispatchTime.now() + 3
+        DispatchQueue.main.asyncAfter( deadline: dispatchTime ) {
+            //ユーザー情報の確認
+            let realm = try! Realm()
+            let users = realm.objects(User.self)
+            if (users.count > 0){
+                print("スプラッシュ画面:ユーザー情報が存在します。メイン画面に遷移します。")
+                self.present(MainViewController(), animated: true, completion: nil)
+            }else{
+                print("スプラッシュ画面:ユーザー情報が存在しません。設定画面に遷移します。")
+                self.present(SettingViewController(), animated: true, completion: nil)
+            }
+        }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    @objc func goNext1(_ sender: UIButton) {// selectorで呼び出す場合Swift4からは「@objc」をつける。
-        let nextvc = SettingViewController()
-        //        nextvc.view.backgroundColor = UIColor.yellow
-        self.present(nextvc, animated: true, completion: nil)
-    }
-    
-    @objc func goNext2(_ sender: UIButton) {// selectorで呼び出す場合Swift4からは「@objc」をつける。
-        let nextvc = MainViewController()
-        //        nextvc.view.backgroundColor = UIColor.blue
-        self.present(nextvc, animated: true, completion: nil)
-    }
-    
-    // Do any additional setup after loading the view, typically from a nib.
-    
-    // 画面サイズを初期値として `MainView` クラスを `mainView` としてインスタンス化します。
-    ////        let mainView = Nav1ViewController()
-    //        let mainView = MainView(frame: self.view.bounds)
-    //
-    //
-    //        // `MainView` に自動サイズ調整用に `autoresizingMask` を設定
-    //        mainView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-    //
-    //        // `mainView` オブジェクトを表示します。
-    //        self.view.addSubview(mainView)
-    //
-    //        let vc = Nav1ViewController()
-    //        let vc2 = Nav2ViewController()
-    //    }
-    
-    
-    
 }
 
